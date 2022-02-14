bcf6064b6dda82fc9171380bd6c679271aa6493a
lideen <lideen@openseas.com.cn>
2022-02-14 13:46:15 +0800

    feat: 网络请求

---
+++ src/utils/request.ts
@@ -0,0 +1,70 @@
+import Taro from '@tarojs/taro'
+
+export const getHeader = () => {
+  const ret: any = {}
+
+  return ret
+}
+
+/**
+ * post 请求
+ * @param path 路径
+ * @param data data
+ * @returns
+ */
+export const doPostAction = ({ url, data }) => {
+  return Taro.request({
+    method: 'POST',
+    url: url,
+    header: getHeader(),
+    data: data,
+    dataType: 'test',
+  })
+}
+
+/**
+ * get 请求
+ * @param path 路径
+ * @param data data
+ * @returns
+ */
+export const doGetAction = ({ url, data }) => {
+  return Taro.request({
+    method: 'GET',
+    url: url,
+    header: getHeader(),
+    data: data,
+    dataType: 'test',
+  })
+}
+
+/**
+ * put 请求
+ * @param path 路径
+ * @param data data
+ * @returns
+ */
+export const doPutAction = ({ url, data }) => {
+  return Taro.request({
+    method: 'PUT',
+    url: url,
+    header: getHeader(),
+    data: data,
+    dataType: 'test',
+  })
+}
+
+/**
+ * delete 请求
+ * @param path 路径
+ * @param data data
+ * @returns
+ */
+export const doDeleteAction = ({ url }) => {
+  return Taro.request({
+    method: 'DELETE',
+    url: url,
+    header: getHeader(),
+    dataType: 'test',
+  })
+}
